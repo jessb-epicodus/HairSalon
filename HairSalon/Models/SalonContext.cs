24cc566d3e9c32de70079f6ca281fb849365cc0a
jb <jessb.epicodus@gmail.com>
2022-03-11 08:54:51 -0800

    update structure to include additional views; update models

--- HairSalon/Models/SalonContext.cs
+++ HairSalon/Models/SalonContext.cs
@@ -2,8 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Models {
   public class HairSalonContext : DbContext {
-    public DbSet<ClassName> Stylists { get; set; }
-    public DbSet<ClassName> Clients { get; set; }
+    public DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
     public HairSalonContext(DbContextOptions options) : base(options) { }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
       optionsBuilder.UseLazyLoadingProxies();
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -2,5 +2,12 @@ using System.Collections.Generic;
 
 namespace HairSalon.Models {
   public class Stylist {
+    public Stylist() {
+      this.Clients = new HashSet<Client>();
+    }
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Specialties { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
   }
 }
